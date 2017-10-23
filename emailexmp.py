7c74ebfcc17e17f33a728279836a1e8c02c91acc
Shubham Kumar <sksinha1289@gmail.com>
2017-10-23 20:38:26 +0530

    Delete emailexmp.py

--- emailexmp.py
+++
@@ -1,8 +0,0 @@
-import sys
-import smtplib
-conn = smtplib.SMTP('smtp.gmail.com', 587)
-conn.ehlo()
-conn.starttls()
-conn.login(sys.argv[1], 'hbpkyxjnuvkdtzll')
-conn.sendmail(sys.argv[1], sys.argv[2], ' '.join(sys.argv[3:]))
-conn.quit()
